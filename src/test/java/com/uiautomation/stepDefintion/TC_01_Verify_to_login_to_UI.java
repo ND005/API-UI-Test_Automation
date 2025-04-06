3850abf6f8a231a475a094b1212dae9a1cfb12d8
ND005 <nimmaladinesh5@gmail.com>
2025-04-06 18:40:40 +0530

    added base URL file

--- src/test/java/com/uiautomation/stepDefintion/TC_01_Verify_to_login_to_UI.java
+++ src/test/java/com/uiautomation/stepDefintion/TC_01_Verify_to_login_to_UI.java
@@ -1,13 +1,21 @@
 package com.uiautomation.stepDefintion;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_01_Verify_to_login_to_UI {
+
+	WebDriver driver = null;
+
 	@Given("^Logging to Swag Labs with (.*) and (.*)$")
 	public void logging_to_swag_labs_with_userID_and_password(String UserID, String Password) {
-
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 	}
 
 	@When("Verify the user profile and home page")
